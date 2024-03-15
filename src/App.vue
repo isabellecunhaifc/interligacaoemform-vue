e8955dc75e8b0fd8fdb49d6b56cd27ee92125630
isabelle.cunha <is4ccunha@gmail.com>
2024-03-15 11:56:40 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+
+const nome = ref('');
+const email = ref('');
+const senha = ref('');
+const confirmacaosenha = ref('');
+</script>
+
+<template>
+  <input type="text" v-model="nome">
+</template>
+
+<style scoped>
+</style>
